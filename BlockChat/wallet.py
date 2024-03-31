9bbf57b1c73e808a300bfcb75d910c790ef13b1f
Manos Emmanouilidis <manos.emmanouilidis05@gmail.com>
2024-03-31 17:55:06 +0300

    Refactor Wallet

--- BlockChat/wallet.py
+++ BlockChat/wallet.py
@@ -2,49 +2,60 @@ from Crypto.PublicKey import RSA
 
 class Wallet:
     """
-    Class that represents a BlockChat wallet
+    Class that represents a BlockChat Wallet.
     
-    private_key     -> wallet's private key
-    public_key      -> wallet's public key
-    nonce           -> number of transactions sent by this wallet
-    transactions    -> a list of all the transaction perfromed by this wallet
+    private_key     -> wallet's private key (RSA)
+    public_key      -> wallet's public key  (RSA)
+    nonce           -> number of transactions sent by the wallet
+    transactions    -> a list of all the transactions of the wallet
     """
     def __init__(self, public_key, private_key):
         self.public_key = public_key
         self.private_key = private_key
         self.transactions = []
         self.nonce = 0
+        self.balance = self.wallet_balance()
         
     def wallet_balance(self):
         """
-        Returns the balance (in BlockChat Coins) of the wallet
+        Computes and returns the balance (in BlockChat Coins) of the wallet.
         """
         balance = 0
-        """
-        TO DO
-        """
+        # TODO: Calculate balance
         return balance
     
+    def add_coins(self, ammount):
+        self.balance += ammount
+    
+    def remove_coins(self, ammount):
+        self.balance -= ammount
+    
+    def increment_nonce(self):
+        """
+        Increments nonce by 1.
+        This method is called for each transaction sent from this wallet.
+        """
+        self.nonce += 1
+        
     def print(self):
         """
-        Prints information about the wallet
+        Prints information about the wallet.
         """
         print('Public Key: \n', self.public_key)
-        print('Private Key: \n', self.private_key)
+        print('Private Key: \n', self.private_key)  # TODO: Remove private key from printing!
         print('Nonce ', self.nonce)
+        print('Balance: ', self.balance)
         print('Transactions: ')
         for transaction in self.transactions:
             transaction.print()
 
 def generate_wallet():
     """
-    It creates a new wallet
+    Creates a new wallet.
     """
     key = RSA.generate(2048)
     
-    private_key = key.export_key()
-    public_key = key.public_key().export_key()
+    private_key = key
+    public_key = key.publickey()
     
     return Wallet(public_key, private_key)
-
-# generate_wallet().print()
