109c620810870f88d817d39d6de467f878909fd4
Manos Emmanouilidis <manos.emmanouilidis05@gmail.com>
2024-04-06 18:25:42 +0300

    Add new field called stake and implement more methods

--- BlockChat/utils.py
+++ BlockChat/utils.py
@@ -1,5 +1,8 @@
 from Crypto.PublicKey import RSA
 from wallet import Wallet
+from transaction import Transaction
+from block import BlockChatCoinBlock
+from blockchain import Blockchain
 
 def generate_wallet():
     """
@@ -11,3 +14,16 @@ def generate_wallet():
     public_key = key.publickey()
     
     return Wallet(public_key, private_key)
+
+def add_transactions_to_wallet(wallet, transactions):
+    """
+    Adds transactions related to the given wallet from a list of transactions.
+
+    Args:
+        wallet (Wallet): The wallet to which transactions are added.
+        transactions (list): A list of Transaction objects.
+    """
+    for transaction in transactions:
+        if transaction.sender_address == wallet.public_key or transaction.receiver_address == wallet.public_key:
+            wallet.transactions.append(transaction)
+            
--- BlockChat/wallet.py
+++ BlockChat/wallet.py
@@ -1,3 +1,5 @@
+from transaction import Transaction
+
 class Wallet:
     """
     Class that represents a BlockChat Wallet.
@@ -6,21 +8,41 @@ class Wallet:
     public_key      -> wallet's public key  (RSA)
     nonce           -> number of transactions sent by the wallet
     transactions    -> a list of all the transactions of the wallet
+    balance         -> wallet's balance (in BCC)
+    stake           -> wallet's stake used for proof of stake
     """
+    
     def __init__(self, public_key, private_key):
         self.public_key = public_key
         self.private_key = private_key
         self.transactions = []
         self.nonce = 0
-        self.balance = self.wallet_balance()
+        self.balance = 0
+        self.stake = 0
         
     def wallet_balance(self):
         """
         Computes and returns the balance (in BlockChat Coins) of the wallet.
         """
         balance = 0
-        # TODO: Calculate balance
+        # self.balance = 0
+        for transaction in self.transactions:
+            if transaction.sender_address == self.public_key:
+                balance -= transaction.cost()
+                # self.remove_coins(transaction.cost())
+            elif transaction.receiver_address == self.public_key and transaction.type == 'coins': 
+                balance += transaction.amount
+                # self.add_coins(transaction.amount)
+        if balance < 0:
+            raise ValueError("Balance shouldn't be negative! Something went wrong! 😕")
         return balance
+    
+    def set_stake(self, amount):
+        if amount <= self.balance:
+            self.stake = amount
+            # TODO: Validator should update the balance 
+        else:
+            raise ValueError("Insufficient balance to stake this amount")
     
     def add_coins(self, ammount):
         self.balance += ammount
@@ -35,6 +57,8 @@ class Wallet:
         """
         self.nonce += 1
         
+        return self.nonce
+                
     def print(self):
         """
         Prints information about the wallet.
@@ -43,6 +67,7 @@ class Wallet:
         print('Private Key: \n', self.private_key)  # TODO: Remove private key from printing!
         print('Nonce ', self.nonce)
         print('Balance: ', self.balance)
+        print('Stake: ', self.stake)
         print('Transactions: ')
         for transaction in self.transactions:
             transaction.print()
