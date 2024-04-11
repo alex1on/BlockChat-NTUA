307fbcb84f9c8fb917e72beadbe465c3e45f24e7
Alexandros Ionitsa <alexandros.ionitsa@gmail.com>
2024-04-11 09:37:28 +0300

    Implement broadcasting

--- BlockChat/node_v2.py
+++ BlockChat/node_v2.py
@@ -34,6 +34,9 @@ class node:
         self.chain = Blockchain()
         self.bootstrap = bootstrap
 
+        self.setup_complete = False
+        self.transaction_queue = []
+
         self.net_nodes = []
 
         if self.bootstrap:
@@ -41,7 +44,7 @@ class node:
             self.next_id = 1
             self.chain = Blockchain(True, N, self.wallet)
             self.bootstraps()
-
+            self.setup_complete = True
         else:
             self.advertise_node("bootstrap_node", 5001)
             for node in self.net_nodes:
@@ -51,6 +54,7 @@ class node:
         self.threads = []
         self.running = True
         self.open_connection("0.0.0.0", port)
+        self.open_connection_broadcast("0.0.0.0", port + 1)
         # self.open_connection(ip, port)
 
     def set_node_id(self, id):
@@ -92,8 +96,11 @@ class node:
         Creates a transaction
         receiver: The node id of the receiving node
         """
-        print(self.id)
-        print(self.net_nodes)
+        # print(self.id)
+        # print(self.net_nodes)
+        if not self.setup_complete:
+            self.transaction_queue.append((type, amount, message, receiver))
+            return
         if self.id == receiver:
             raise Exception("Recipient can't be the sender.")
         if amount and amount <= 0:
@@ -103,7 +110,12 @@ class node:
 
         nonce = self.wallet.increment_nonce()
         transaction = Transaction(
-            self.wallet.public_key, RSA.import_key(receiver_node["public_key"].encode()), type, nonce, amount, message
+            self.wallet.public_key,
+            RSA.import_key(receiver_node["public_key"].encode()),
+            type,
+            nonce,
+            amount,
+            message,
         )
         # self.wallet.transactions.append(transaction)
         # Sign the transaction
@@ -111,7 +123,7 @@ class node:
 
         # TODO: Names are missleading change either node blockchain name or block list on blockchain
         self.chain.chain[-1].add_transaction(transaction)
-        self.chain.print()
+        # self.chain.print()
 
         # Broadcast the transaction
         self.broadcast_transaction(receiver_node, transaction)
@@ -134,7 +146,7 @@ class node:
             (
                 node
                 for node in self.net_nodes
-                    if node["public_key"] == transaction.sender_address
+                if node["public_key"] == transaction.sender_address
             ),
             None,
         )
@@ -159,11 +171,17 @@ class node:
         """
         message = {
             "type": "broadcast_transaction",
-            "transaction": transaction.to_json()
+            "transaction": transaction.to_json(),
         }
-        send_message(receiver_node["ip"], receiver_node["port"], message)
+        # send_message(receiver_node["ip"], receiver_node["port"], message)
+        send_message_broadcast(receiver_node["port"] + 1, message)
         # for node in self.network:
         #     pass
+
+    def process_transaction_queue(self):
+        for transaction in self.transaction_queue:
+            self.create_transaction(*transaction)
+        self.transaction_queue = []
 
     def broadcast_block(self, receiver_node=0, block=None):
         """
@@ -173,11 +191,9 @@ class node:
         self.chain.add_block(block)
         receiver_node = self.net_nodes[receiver_node]
 
-        message = {
-            "type": "broadcast_block",
-            "block": block.to_json()
-        }
-        send_message(receiver_node["ip"], receiver_node["port"], message)
+        message = {"type": "broadcast_block", "block": block.to_json()}
+        # send_message(receiver_node["ip"], receiver_node["port"], message)
+        send_message_broadcast(receiver_node["port"] + 1, message)
 
     def stake(self, amount):
         """
@@ -253,28 +269,31 @@ class node:
         self.next_id += 1
 
         message = {"type": "set up ready", "nodes": self.net_nodes}
+        with open("output.txt", "a") as f:
+            print(message, file=f)
 
         # TODO: send_message on network info should only be called when all the nodes are inserted in the network.
         # TODO: also the send_message function in this case should broadcast the message instead of simply sending it to a specific host.
         # INFO: Check multicast implementation on udp / tcp packets.
-        #if len(self.net_nodes) == self.N:
-        send_message(data["node"]["ip"], data["node"]["port"], message)
+        if len(self.net_nodes) >= self.N:
+            send_message_broadcast(self.port + 1, message)
+        # send_message(data["node"]["ip"], data["node"]["port"], message)
 
     def node_finish_init(self, data):
         self.set_node_id(data["id"])
         self.chain = Blockchain.from_json(data["blockchain"])
-        print(self.id)
-        self.chain.print()
+        # print(self.id)
+        # self.chain.print()
 
     def handle_new_transaction(self, data):
         transaction = Transaction.from_json(data["transaction"])
         if self.validate_transaction(transaction):
             self.chain.chain[-1].add_transaction(transaction)
-            self.chain.print()
+            # self.chain.print()
 
     def handle_new_block(self, data):
         block = BlockChatCoinBlock.from_json(data["block"])
-        #if block.validate_block(self.chain.chain[-1], 0):
+        # if block.validate_block(self.chain.chain[-1], 0):
         self.chain.add_block(block)
         self.chain.print()
 
@@ -288,6 +307,8 @@ class node:
         elif data["type"] == "set up ready":
             self.net_nodes = data["nodes"]
             print(json.dumps(self.net_nodes, sort_keys=True, indent=4))
+            self.setup_complete = True
+            self.process_transaction_queue()
         elif data["type"] == "broadcast_transaction":
             self.handle_new_transaction(data)
         elif data["type"] == "broadcast_block":
@@ -307,6 +328,8 @@ class node:
                 print(f"Received from {addr}: {data}")
                 # blockchain.add_block(data)
                 # print(data)
+                with open("output_received.txt", "a") as f:
+                    print(data, file=f)
 
                 # TODO: The way it works right now is not ideal. Fix inc.
                 self.handle_client_response(addr[0], addr[1], data)
@@ -317,10 +340,13 @@ class node:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.server_socket:
             self.server_socket.bind((host, port))
             self.server_socket.listen()
-            print(f"Bootstrap node listening on {host}:{port}")
+            if self.bootstrap:
+                print(f"Bootstrap listening on {host}:{port}")
+            else:
+                print(f"Node listening on {host}:{port}")
 
             while self.running:
-                print("here")
+                # print("here")
                 # self.server_socket.settimeout(1.0)  # Allow checking the running flag -- comments: might not be needed
                 try:
                     conn, addr = self.server_socket.accept()
@@ -340,6 +366,34 @@ class node:
         )
         server_thread.start()
 
+    def open_connection_broadcast(self, host, port):
+        """Initializes the UDP socket for listening and sending."""
+        self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # Allow reusing addresses
+        self.udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # Enable broadcasting mode
+        self.udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.udp_socket.bind((host, port))
+        print(f"Node listening for UDP messages on port {port}")
+        # Start listening in a new thread
+        threading.Thread(target=self.listen_for_udp_messages, daemon=True).start()
+
+    def listen_for_udp_messages(self):
+        """Listens for messages from other nodes and processes them."""
+        while self.running:
+            try:
+                data, addr = self.udp_socket.recvfrom(
+                    4096
+                )  # Adjust buffer size as needed
+                if data:
+                    message_data = json.loads(data.decode())
+                    print(f"Received message from {addr}: {message_data}")
+                    # Process the message
+                    self.handle_client_response(addr[0], addr[1], message_data)
+            except Exception as e:
+                print(f"Error receiving UDP message: {e}")
+                break
+
     def shutdown(self):
         print("Shutting down the server...")
         self.running = False
@@ -352,16 +406,52 @@ class node:
         print("Signal received, shutting down...")
         self.shutdown()
         sys.exit(0)
-    
+
     def dummy_block_creator(self):
-        block = BlockChatCoinBlock(len(self.chain.chain) + 1, [], self.chain.chain[-1].hash)
-        block.add_transaction(Transaction(self.wallet.public_key, RSA.import_key(self.net_nodes[0]["public_key"].encode()), "coins", 1, 10))
-        block.add_transaction(Transaction(self.wallet.public_key, RSA.import_key(self.net_nodes[0]["public_key"].encode()), "coins", 2, 20))
+        block = BlockChatCoinBlock(
+            len(self.chain.chain) + 1, [], self.chain.chain[-1].hash
+        )
+        block.add_transaction(
+            Transaction(
+                self.wallet.public_key,
+                RSA.import_key(self.net_nodes[0]["public_key"].encode()),
+                "coins",
+                1,
+                10,
+            )
+        )
+        block.add_transaction(
+            Transaction(
+                self.wallet.public_key,
+                RSA.import_key(self.net_nodes[0]["public_key"].encode()),
+                "coins",
+                2,
+                20,
+            )
+        )
 
         return block
 
+
 # TODO: Implement the broadcast method
-def send_message_broadcast(data):
+def send_message_broadcast(port, data):
+    broadcast_address = (
+        "172.20.255.255"  # Broadcast address for the 172.20.0.0/16 subnet
+    )
+    serialized_data = json.dumps(data).encode()
+
+    with open("output.txt", "a") as f:
+        print(serialized_data, file=f)
+
+    # Create a UDP socket
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        # Set the option to allow broadcasting
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        # Send the data to the broadcast address
+        sock.sendto(serialized_data, (broadcast_address, port))
+        print(f"Broadcasted message to {broadcast_address}:{port}")
+        with open("output.txt", "a") as f:
+            print(f"Broadcasted message to {broadcast_address}:{port}", file=f)
     return
 
 
@@ -371,7 +461,7 @@ def send_message(host, port, data):
             client_socket.connect((host, port))
             print(f"Connected to {host}:{port}")
             client_socket.sendall(json.dumps(data).encode())
-            print(f"JSON data sent: {json.dumps(data).encode()}")
+            # print(f"JSON data sent: {json.dumps(data).encode()}")
 
     except Exception as e:
         print(f"An error occurred: {e}")
--- BlockChat/start_node.py
+++ BlockChat/start_node.py
@@ -1,6 +1,7 @@
 import signal
 import time
 import sys
+import socket
 from node_v2 import node
 
 if len(sys.argv) < 2:
@@ -9,7 +10,16 @@ if len(sys.argv) < 2:
 
 port = int(sys.argv[1])
 suffix = port - 5001
-node_instance = node(f"simple_node{suffix}", port, 3)
+
+# Get the container's hostname (which is also the container's name within the Docker network)
+hostname = socket.gethostname()
+# Resolve the hostname to an IP address
+ip_address = socket.gethostbyname(hostname)
+print(ip_address)
+
+# node_instance = node(f"simple_node{suffix}", port, 3)
+node_instance = node(ip_address, port, 3)
+
 signal.signal(signal.SIGINT, node_instance.signal_handler)
 time.sleep(2)
 node_instance.create_transaction("coins", 10, "", 0)
